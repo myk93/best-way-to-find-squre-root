e2a957770d777c7722941f0db25798b96d3d69ad
menny kroizer <meni.kroizer@gmail.com>
2020-02-17 21:36:18 +0200

    creating the program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,55 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static double Avrage(double a, double b)
+        {
+            return ((a + b) / (double)2);
+        }
+
+        static double Divide_by_avrage(double num,double avrage)
+        {
+            return (num / (double)avrage);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter a real number above 0");
+            double num = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter the range of answuer (between 0-1) ");
+            double digit =double.Parse( Console.ReadLine());
+
+            double min = 1;
+            double max = num;
+
+            int count = 0;
+            while ((max-min)>= digit )
+            {
+                count++;
+                double tempMin = Avrage(min, max);
+                double tempMax = Divide_by_avrage(num, tempMin);
+
+                if (tempMin>tempMax)
+                {
+                    max = tempMin;
+                    min = tempMax;
+                }
+                else
+                {
+                    max = tempMax;
+                    min = tempMin;
+                }
+            }
+
+            if (Math.Floor(max) * Math.Floor(max) == num)
+            {
+                Console.WriteLine("the root is {0}",Math.Floor(max));
+            }
+            else
+            {
+                Console.WriteLine("the root is between {0} and {1}", min, max);
+                Console.WriteLine(count);
+            }
+            
+            
         }
     }
 }
